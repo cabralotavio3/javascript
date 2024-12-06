dbafc60c92ee10e0a734b3c1425b6d0a998d70d1
cabralotavio3 <bugvug250@gmail.com>
2024-12-06 10:31:37 -0300

    JAVA SCRIPT COM TYPE SCRIPT

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header"
+import { Footer } from "./components/Footer"
+export function App() {
+  return (
+    <>
+      <Header />
+      <h1>REACAO</h1>
+      < Footer />
+    </>
+
+  )
+}
